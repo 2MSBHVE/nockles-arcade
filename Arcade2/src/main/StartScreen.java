c878b8d08014d32443d16ab9498de9f19dea1fb9
2MSBHVE <max@miku.nyc>
2017-04-02 18:35:09 -0400

    minor timing change

--- Arcade2/src/main/StartScreen.java
+++ Arcade2/src/main/StartScreen.java
@@ -82,117 +82,130 @@ public class StartScreen extends ClickableScreen {
 		futureButton = new Button(25, getHeight() - 40, 290, 30, "  RETURN TO THE PRESENT", new Color(0, 127, 127), new Action() {
 			@SuppressWarnings("deprecation")
 			public void act() {
-//				Window x = StartProgram.sPrg;
-//				StartProgram.sPrg.dispose();
-				
-				
-				StartProgram.keepPlaying = false;
-				StartProgram.clip.stop();
-
-
-				new Thread() {
-					public void run() {
-
-						try {
-
-							Clip clip = AudioSystem.getClip();
-							clip.open(AudioSystem.getAudioInputStream(StartProgram.class.getResource("./end.wav")));
-							clip.start();
-							Thread.sleep(22100);
-						} catch (UnsupportedAudioFileException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						} catch (LineUnavailableException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-
-						System.exit(0);
-					}
-				}.start(); 
-				new Thread() {
-					public void run() {
-						
-//						StartProgram.sPrg.setName("Corrupted");
-						boolean b = true;
-						while(true){
-//							boolean b = (Math.random() < 0.5);
-							b = !b;
-							if(b){
-								StartProgram.sPrg.setTitle("WARNING!");
-							} else {
-								StartProgram.sPrg.setTitle("Program Corrupted");
-							}
-
-//							StartProgram.sPrg.repaint();
-							
-//							StartProgram.sPrg.update(null);
-							
-						    
-							try {
-								Thread.sleep(750);
-							} catch (InterruptedException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-						}
-					}
-				}.start();
-				
-				new Thread() {
-					public void run() {
-						try {
-							Thread.sleep(7871);
-						} catch (InterruptedException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-						
-//						
-						
-						boolean b = true;
-						while(true){
-//							boolean b = (Math.random() < 0.5);
-							b = !b;
-							StartProgram.sPrg.setVisible(b);
-//							StartProgram.sPrg.repaint();
-							
-//							StartProgram.sPrg.update(null);
-							
-						    
-							try {
-								Thread.sleep((long) ((Math.random() * 125)));
-							} catch (InterruptedException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-						}
-					}
-				}.start(); 
-				
-				try {
-					Thread.sleep(19452);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				while(true){
-					JOptionPane.showMessageDialog(null, "Returning to the future... \n\n"
-							+ "Incoming message from 1995: \n"
-							+ "Hope you had fun! But before you go, just remember... \n"
-							+ "Don't do anything stupid like elect some billionaire as President or something...");
-				}
+				endSequence();
 			}
 		});
 		
 		viewObjects.add(futureButton);
 		
+	}
+	
+	private void endSequence(){
+//		Window x = StartProgram.sPrg;
+//		StartProgram.sPrg.dispose();
+		
+		
+		StartProgram.keepPlaying = false;
+		StartProgram.clip.stop();
+
+		new Thread() {
+			public void run() {
+				viewObjects.remove(startButton);
+			}
+		}.start();
+		
+		new Thread() {
+			public void run() {
+
+				try {
+
+					Clip clip = AudioSystem.getClip();
+					clip.open(AudioSystem.getAudioInputStream(StartProgram.class.getResource("./end.wav")));
+					clip.start();
+					Thread.sleep(22200);
+				} catch (UnsupportedAudioFileException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (LineUnavailableException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				System.exit(0);
+			}
+		}.start(); 
+		
+		new Thread() {
+			public void run() {
+				
+				
+				
+//				StartProgram.sPrg.setName("Corrupted");
+				boolean b = true;
+				while(true){
+//					boolean b = (Math.random() < 0.5);
+					b = !b;
+					if(b){
+						StartProgram.sPrg.setTitle("WARNING!");
+					} else {
+						StartProgram.sPrg.setTitle("Program Corrupted");
+					}
+
+//					StartProgram.sPrg.repaint();
+					
+//					StartProgram.sPrg.update(null);
+					
+				    
+					try {
+						Thread.sleep(750);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}.start();
+		
+		new Thread() {
+			public void run() {
+				try {
+					Thread.sleep(7871);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+//				
+				
+				boolean b = true;
+				while(true){
+//					boolean b = (Math.random() < 0.5);
+					b = !b;
+					StartProgram.sPrg.setVisible(b);
+//					StartProgram.sPrg.repaint();
+					
+//					StartProgram.sPrg.update(null);
+					
+				    
+					try {
+						Thread.sleep((long) ((Math.random() * 125)));
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}.start(); 
+		
+		try {
+			Thread.sleep(19452);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		while(true){
+			JOptionPane.showMessageDialog(null, "Returning to the future... \n\n"
+					+ "Incoming message from 1995: \n"
+					+ "Hope you had fun! But before you go, just remember... \n"
+					+ "Don't do anything stupid like elect some billionaire as President or something...");
+		}
+	
 	}
 
 	public void repaintUserName() {
